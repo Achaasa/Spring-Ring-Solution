ddde933fba3cc517ac305ae0b3a411b2730ba65b
achaasa <josephachaasa@gmail.com>
2025-08-14 01:59:16 +0000

    Remove migration files for initial database schema

--- prisma/migrations/20250706232045_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `status` column on the `Payment` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Payment" DROP COLUMN "status",
-ADD COLUMN     "status" TEXT NOT NULL DEFAULT 'PENDING';
--- prisma/migrations/20250707004333_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Booking" ADD COLUMN     "paymentStatus" "PaymentStatus" NOT NULL DEFAULT 'PENDING';
-
--- AlterTable
-ALTER TABLE "Payment" ADD COLUMN     "method" TEXT;
